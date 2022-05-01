27df1ba3d185c43914af7433c4683c8ada45084a
Rune Ludvigsen <rune.ludvigsen@hotmail.com>
2022-05-01 12:55:22 +0200

    Added End scenes

---
+++ Assets/Scripts/UI Elements/fadeoutending.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class fadeoutending : MonoBehaviour
+{
+    private float faded = 0.25f;
+    public Image img;
+    public int wait;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fade());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator fade()
+    {
+        yield return new WaitForSeconds(wait);
+        while (faded < 1)
+        {
+            print(faded);
+            img.color = new Color(img.color.r, img.color.g, img.color.b, faded);
+            faded += .0001f;
+            yield return null;
+        }
+        SceneManager.LoadScene("EndCredits");
+        yield return null;
+    }
+}
